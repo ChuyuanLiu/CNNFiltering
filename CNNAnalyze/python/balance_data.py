c3456bf83b045a002acc1d973f907652d8ed6784
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-02-20 02:15:02 +0100

    Trying dataset Python file

---
+++ CNNAnalyze/python/balance_data.py
@@ -0,0 +1,97 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import sys, time
+import argparse
+
+from math import floor
+
+headLab = ["run","evt","detSeqIn","detSeqOut","bSX","bSY","bSZ","bSdZ"]
+
+hitCoord = ["X","Y","Z","Phi","R"]
+
+hitDet = ["DetSeq","IsBarrel","Layer","Ladder","Side","Disk","Panel","Module","IsFlipped","Ax1","Ax2"]
+
+hitClust = ["ClustX","ClustY","ClustSize","ClustSizeX","ClustSizeY","PixelZero",
+            "AvgCharge","OverFlowX","OverFlowY","IsBig","IsBad","IsEdge"]
+
+hitPixel = ["Pix" + str(el) for el in range(1, 257)]
+
+hitCharge = ["SumADC"]
+
+hitLabs = hitCoord + hitDet + hitClust + hitPixel + hitCharge
+
+inHitLabs = [ "in" + str(i) for i in hitLabs]
+outHitLabs = [ "out" + str(i) for i in hitLabs]
+
+particleLabs = ["label","tpKey","px","py","pz","pt","mT","eT","mSqr","pdgId",
+                "charge","nTrackerHits","nTrackerLayers","phi","eta","rapidity"
+                "vX","vY","vZ","dXY","dZ","bunchCrossing"]
+
+dataLab = headLab + inHitLabs + particleLabs + particleLabs + ["dummyFlag"]
+
+
+import pandas as pd
+import numpy as np
+
+
+def npDoubletsLoad(path,fileslimit,cols):
+    print ("======================================================================")
+
+    start = time.time()
+
+    datafiles = np.array([f for f in listdir(path) if (isfile(join(path, f)) and  f.lower().endswith(("txt","gz")) and "_dataset." in f)])
+
+    print("Loading " + str(len(datafiles)) + " dataset file(s) . . .")
+
+    idName = ""
+
+    for p in path.split("/"):
+        if "runs" in p:
+            idName = p
+
+    singlePath = path + "/singleEvts/"
+    if not os.path.exists(singlePath):
+        os.makedirs(singlePath)
+
+    listdata = []
+    for no,d in enumerate(datafiles):
+        if os.stat(path + d).st_size == 0:
+                print("File no." + str(no+1) + " " + d + " empty.Skipping.")
+                continue
+        with open(path + d, 'rb') as df:
+            print("Reading file no." + str(no+1) + ": " + d)
+            if d.lower().endswith(("txt")):
+                dfDoublets = pd.read_table(df, sep="\t", header = None)
+            if d.lower().endswith(("gz")):
+                dfDoublets = pd.read_table(df, sep="\t", header = None,compression="gzip")
+            if cols:
+                dfDoublets.columns = dataLab
+            #print(dfDoublets.head())
+            dfDoublets.to_hdf(singlePath + idName + "_" + d.replace(".txt",".h5"),'data',append=True)
+            listdata.append(dfDoublets)
+
+    alldata = pd.concat(listdata)
+
+    dfDoublets.to_hdf(path + idName + "_" + "doublets.h5",'data',append=True)
+
+    end = time.time()
+    print ("======================================================================")
+    print ("\n - Timing : " + str(end-start))
+
+    return alldata
+
+
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(prog="dataToHDF")
+    parser.add_argument('--read', type=str, default="./",help='files path')
+    parser.add_argument('--flimit', type=int, default=-1,help='max no. of files')
+    parser.add_argument('--columns', type=bool, default=False,help='columns?')
+    #parser.add_argument('--debug', type=bool, default=False,help='debug printouts')
+    args = parser.parse_args()
+
+    npDoubletsLoad(args.read,args.flimit,args.colums)
---
+++ CNNAnalyze/python/dataset.py
@@ -0,0 +1,232 @@
+
+def to_categorical(y, num_classes=None):
+    """Converts a class vector (integers) to binary class matrix.
+    E.g. for use with categorical_crossentropy.
+    # Arguments
+        y: class vector to be converted into a matrix
+            (integers from 0 to num_classes).
+        num_classes: total number of classes.
+    # Returns
+        A binary matrix representation of the input.
+    """
+    y = np.array(y, dtype='int').ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes))
+    categorical[np.arange(n), y] = 1
+    return categorical
+
+
+target_lab = "pdgId"
+
+headLab = ["run","evt","detSeqIn","detSeqOut","bSX","bSY","bSZ","bSdZ"]
+
+hitCoord = ["X","Y","Z","Phi","R"]
+
+hitDet = ["DetSeq","IsBarrel","Layer","Ladder","Side","Disk","Panel","Module","IsFlipped","Ax1","Ax2"]
+
+hitClust = ["ClustX","ClustY","ClustSize","ClustSizeX","ClustSizeY","PixelZero",
+            "AvgCharge","OverFlowX","OverFlowY","IsBig","IsBad","IsEdge"]
+
+hitPixel = ["Pix" + str(el) for el in range(1, 257)]
+
+hitCharge = ["SumADC"]
+
+hitLabs = hitCoord + hitDet + hitClust + hitPixel + hitCharge
+
+inHitLabs = [ "in" + str(i) for i in hitLabs]
+outHitLabs = [ "out" + str(i) for i in hitLabs]
+
+particleLabs = ["label","tpKey","px","py","pz","pt","mT","eT","mSqr","pdgId",
+                "charge","nTrackerHits","nTrackerLayers","phi","eta","rapidity"
+                "vX","vY","vZ","dXY","dZ","bunchCrossing"]
+
+dataLab = headLab + inHitLabs + outHitLabs + particleLabs + particleLabs + ["dummyFlag"]
+
+
+class Dataset:
+    """ Load the dataset from txt files. """
+
+    def __init__(self, fnames):
+        self.data = pd.DataFrame(data=[], columns=dataLab)
+        for i,f in enumerate(fnames):
+            if not f.lower().endswith("h5"):
+                continue
+
+            print("Loading file " + str(i+1) + "/" + str(len(fnames)) + " : " + f)
+            df = 0
+            df = pd.read_hdf(f, mode='r')
+            df.columns = dataLab  # change wrong columns names
+            self.data = self.data.append(df)
+
+    def from_dataframe(data):
+        """ Constructor method to initialize the classe from a DataFrame """
+        d = Dataset([])
+        d.data = data
+        return d
+
+    def theta_correction(self, hits_in, hits_out):
+        # theta correction
+        cosThetaIns = np.cos(np.arctan(np.multiply(
+            self.data["inY"], 1.0 / self.data["inZ"])))
+        cosThetaOuts = np.cos(np.arctan(np.multiply(
+            self.data["outY"], 1.0 / self.data["outZ"])))
+        sinThetaIns = np.sin(np.arctan(np.multiply(
+            self.data["inY"], 1.0 / self.data["inZ"])))
+        sinThetaOuts = np.sin(np.arctan(np.multiply(
+            self.data["outY"], 1.0 / self.data["outZ"])))
+
+        inThetaModC = np.multiply(hits_in, cosThetaIns[:, np.newaxis])
+        outThetaModC = np.multiply(hits_out, cosThetaOuts[:, np.newaxis])
+
+        inThetaModS = np.multiply(hits_in, sinThetaIns[:, np.newaxis])
+        outThetaModS = np.multiply(hits_out, sinThetaOuts[:, np.newaxis])
+        return inThetaModC, outThetaModC, inThetaModS, outThetaModS
+
+    def separate_flipped_hits(self, hit_shapes, flipped):
+        flipped = flipped.astype('bool')
+        flipped_hits = np.zeros(hit_shapes.shape)
+        not_flipped_hits = np.zeros(hit_shapes.shape)
+        flipped_hits[flipped, :] = hit_shapes[flipped, :]
+        not_flipped_hits[~flipped, :] = hit_shapes[~flipped, :]
+        return flipped_hits, not_flipped_hits
+
+    def get_hit_shapes(self, normalize=True, angular_correction=True, flipped_channels=True):
+        """ Return hit shape features
+        Args:
+        -----
+            normalize : (bool)
+                normalize the data matrix with zero mean and unitary variance.
+        """
+        a_in = self.data[inhitlabs].as_matrix()
+        a_out = self.data[outhitlabs].as_matrix()
+
+        # Normalize data
+        if normalize:
+            # mean, std precomputed for data NOPU
+            mean, std = (668.25684, 3919.5576)
+            a_in = a_in / std
+            a_out = a_out / std
+
+        if flipped_channels:
+            flip_in, not_flip_in = self.separate_flipped_hits(
+                a_in, self.data.isFlippedIn)
+            flip_out, not_flip_out = self.separate_flipped_hits(
+                a_out, self.data.isFlippedOut)
+            l = [flip_in, not_flip_in, flip_out, not_flip_out]
+        else:
+            l = [a_in, a_out]
+        if angular_correction:
+            thetac_in, thetac_out, thetas_in, thetas_out = self.theta_correction(
+                a_in, a_out)
+            l = l + [thetac_in, thetac_out, thetas_in, thetas_out]
+
+        data = np.array(l)  # (channels, batch_size, hit_size)
+        data = data.reshape((len(data), -1, 15, 15))
+        # TODO: not optimal for CPU execution
+        return np.transpose(data, (1, 2, 3, 0))
+
+    def filter(self, feature_name, value):
+        """ filter data keeping only those samples where s[feature_name] = value """
+        self.data = self.data[self.data[feature_name] == value]
+        return self  # to allow method chaining
+    def Filter(self, feature_name, value):
+        """ filter data keeping only those samples where s[feature_name] = value """
+        d = Dataset(self.data[self.data[feature_name] == value])
+        d.data =  self.data[self.data[feature_name] == value]
+        return d  # to allow method chaining
+
+    def get_info_features(self):
+        """ Returns info features as numpy array. """
+        return self.data[featurelabs].as_matrix()
+
+    def get_layer_map_data(self):
+        a_in = self.data[inhitlabs].as_matrix().astype(np.float16)
+        a_out = self.data[outhitlabs].as_matrix().astype(np.float16)
+
+        # mean, std precomputed for data NOPU
+        mean, std = (668.25684, 3919.5576)
+        a_in = a_in / std
+        a_out = a_out / std
+
+        l = []
+        thetac_in, thetac_out, thetas_in, thetas_out = self.theta_correction(
+            a_in, a_out)
+        l = l + [thetac_in, thetac_out, thetas_in, thetas_out]
+
+        for hits, ids in [(a_in, self.data.detSeqIn), (a_out, self.data.detSeqOut)]:
+
+            for id_layer in layer_ids:
+                layer_hits = np.zeros(hits.shape)
+                bool_mask = ids == id_layer
+                layer_hits[bool_mask, :] = hits[bool_mask, :]
+                l.append(layer_hits)
+
+        data = np.array(l)  # (channels, batch_size, hit_size)
+        data = data.reshape((len(data), -1, 15, 15))
+        X_hit = np.transpose(data, (1, 2, 3, 0))
+
+        #print(X_hit[0,:,:,0])
+
+        X_info = self.get_info_features()
+        y = to_categorical(self.get_labels())
+        return X_hit, X_info, y
+
+    def get_labels(self):
+        return self.data[target_lab].as_matrix() != -1.0
+
+    def get_data(self, normalize=True, angular_correction=True, flipped_channels=True):
+        X_hit = self.get_hit_shapes(
+            normalize, angular_correction, flipped_channels)
+        X_info = self.get_info_features()
+        y = to_categorical(self.get_labels(), num_classes=2)
+        return X_hit, X_info, y
+
+    def save(self, fname):
+        # np.save(fname, self.data.as_matrix())
+        self.data.to_hdf(fname, 'data', mode='w')
+
+    # TODO: pick doublets from same event.
+    def balance_data(self, max_ratio=0.5, verbose=True):
+        """ Balance the data. """
+        data_neg = self.data[self.data[target_lab] == -1.0]
+        data_pos = self.data[self.data[target_lab] != -1.0]
+
+        n_pos = data_pos.shape[0]
+        n_neg = data_neg.shape[0]
+
+        if n_pos==0:
+                print("Number of negatives: " + str(n_neg))
+                print("Number of positive: " + str(n_pos))
+                print("Returning")
+                return self
+        if verbose:
+            print("Number of negatives: " + str(n_neg))
+            print("Number of positive: " + str(n_pos))
+            print("Ratio: " + str(n_neg / n_pos))
+
+        if n_pos > n_neg:
+            return self
+
+        data_neg = data_neg.sample(n_pos)
+        balanced_data = pd.concat([data_neg, data_pos])
+        balanced_data = balanced_data.sample(frac=1)  # Shuffle the dataset
+        self.data = balanced_data
+        return self  # allow method chaining
+
+
+if __name__ == '__main__':
+    d = Dataset('data/debug.npy')
+    batch_size = d.data.as_matrix().shape[0]
+
+    x = d.get_data()
+    assert x[0].shape == (batch_size, 15, 15, 8)
+
+    x = d.get_data(normalize=False, angular_correction=False,
+                   flipped_channels=False)[0]
+    assert x.shape == (batch_size, 15, 15, 2)
+    np.testing.assert_allclose(
+        x[:, :, :, 0], d.data[inhitlabs].as_matrix().reshape((-1, 15, 15)))
+
+    print("All test successfully completed.")
