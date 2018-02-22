7995ae0c8384c2b30c316b815061ea0a30611e10
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-02-22 07:22:57 +0100

    Adding path input parser

--- CNNAnalyze/python/layer_maps_model.py
+++ CNNAnalyze/python/layer_maps_model.py
@@ -24,8 +24,8 @@ def batch_generator(data_df,s):
 	dsize = data_df.shape[0]
 	print("Whole data size : " + str(dsize))
 	print("Batch size      : " + str(s))
-	slast = int(dsize / s) 
-	print("No. batches     : " + str(slast + 1))   
+	slast = int(dsize / s)
+	print("No. batches     : " + str(slast + 1))
  	#x_hit = data_list[0]
 	#x_inf = data_list[1]
     #len(data_df)
@@ -33,15 +33,15 @@ def batch_generator(data_df,s):
 #		for i in range(slast+1): # 1875 * 32 = 60000 -> # of traini
             		#if i%10==0:
             		#    print "i = " + str(i)
-		
-		index= random.choice(len(s),1)	            		
+
+		index= random.choice(len(s),1)
 		df_i = data_df[index, :]
         	print(df_i.shape[0])
 		doublets = Dataset([])
             	doublets = Dataset.from_dataframe(df_i)
             	X_hit, X_info, y = doublets.get_layer_map_data()
-        	X_list = [X_hit,X_info]    
-           	print(X_hit.shape[0])	
+        	X_list = [X_hit,X_info]
+           	print(X_hit.shape[0])
 		yield (X_list,y)
 
 DEBUG = os.name == 'nt'  # DEBUG on laptop
@@ -58,6 +58,7 @@ t_now = '{0:%Y-%m-%d_%H-%M-%S}'.format(datetime.datetime.now())
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_epochs', type=int, default=200 if not DEBUG else 3,
                     help='number of epochs')
+parser.add_argument('--path',type=str,default="data/bal_data/")
 parser.add_argument('--batch_size', type=int, default=1024)
 parser.add_argument('--dropout', type=float, default=0.5)
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
@@ -91,23 +92,23 @@ if not os.path.exists(args.log_dir + "/" + str(t_now)):
     os.makedirs(args.log_dir + "/" + str(t_now))
 if not os.path.exists(log_dir_tf):
     os.makedirs(log_dir_tf)
-    
-fname = args.log_dir + "/" + str(t_now) + "/" + args.name  
+
+fname = args.log_dir + "/" + str(t_now) + "/" + args.name
 
 if args.phi is not None:
 	fname = fname + "_phi"
-        
+
 # "/eos/cms/store/cmst3/group/dehep/convPixels/TTBar_13TeV_PU35/"
-remote_data = "data/bal_data/"
+
+remote_data = args.path
 #debug_data = ['data/h5data/' + el for el in ['doublets.h5', 'doublets2.h5']]
-debug_data = "data/bal_data/debug/"
+debug_data = remote_data + "/debug/"
 
 debug_files = [ debug_data + el for el in os.listdir(debug_data)]
 
-print(debug_files)
 
 print("Loading data...")
-train_files = ['data/bal_data/train/' +
+train_files = [remote_data + '/train/' +
                el for el in os.listdir(remote_data + 'train/')] if not args.debug else debug_files
 val_files = [remote_data + 'val/' +
              el for el in os.listdir(remote_data + 'val/')] if not args.debug else debug_files
@@ -148,15 +149,15 @@ if not args.multiclass:
 else:
     X_val_hit, X_val_info, y_val = val_data.get_layer_map_data_multiclass()
     X_test_hit, X_test_info, y_test = test_data.get_layer_map_data_multiclass()
-    
+
 while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < args.gepochs).any():
     thisindices = indices[i*args.fsamp:(i+1)*args.fsamp]
-    
+
     endindices = list(itertools.chain(endindices,thisindices))
     train_batch_file = np.take(train_files,thisindices)
-    
+
     train_data = Dataset(train_batch_file)
-    
+
     if args.verbose:
         print("Iteration no. " + str(i) + " on " + str(nochunks))
         print("Using " + str(len(train_batch_file)) + " files with IDs : " + str(thisindices))
@@ -165,7 +166,7 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
         print("Tot no. of files : " + str(len(train_files)))
         print("Files done       : " + str(np.sum(donechunks)))
         print("Global epochs    : " + str(np.sum(donechunks)/len(train_files)))
-        
+
     flipping = args.flip
 
     #train_data = train_data.filter('isFlippedIn', flipping).filter('isFlippedOut', flipping).balance_data()
@@ -177,7 +178,7 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
     #test_data = test_data
 
     print("loading train data . . .")
-    
+
     if not args.multiclass:
         if args.phi is not None:
             X_hit, X_info, y = train_data.get_layer_map_data_withphi()
@@ -185,7 +186,7 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
             X_hit, X_info, y = train_data.get_layer_map_data()
     else:
         X_hit, X_info, y = train_data.get_layer_map_data_multiclass()
-    
+
     print("Training size: " + str(train_data.data.shape[0]))
     print("Val size: " + str(X_val_hit.shape[0]))
     print("Test size: " + str(X_test_hit.shape[0]))
@@ -198,17 +199,17 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
     test_input_list = [X_test_hit, X_test_info]
 
     #print()
-    
+
     if not args.multiclass:
         model = small_doublet_model(args,train_input_list[0].shape[-1])
     else:
         model = small_doublet_model(args,train_input_list[0].shape[-1],len(pdg)+2)
-        
+
     if args.verbose and i==0:
-        model.summary() 
-        
+        model.summary()
+
     print('Training')
-          
+
     if np.sum(donechunks)/len(train_files) >0.0:
         print("loading weights from iteration " + str(i-1) + " from " + fname)
         model.load_weights(fname + ".h5")
@@ -216,7 +217,7 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
         if args.loadw is not None and os.path.isfile(args.loadw):
             print("loading weights from previous run from" + args.loadw)
             model.load_weights(args.loadw)
-        
+
     with open(fname + ".json", "w") as outfile:
         json.dump(model.to_json(), outfile)
 
@@ -232,7 +233,7 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
     #model.fit_generator(batch_generator(train_data.data,args.bsamp),samples_per_epoch = args.bsamp , verbose=args.verbose,callbacks=callbacks,validation_data=(val_input_list, y_val),nb_epoch=args.n_epochs)
 
     history = model.fit(train_input_list, y, batch_size=args.batch_size, epochs=args.n_epochs, shuffle=True,validation_data=(val_input_list, y_val), callbacks=callbacks, verbose=args.verbose)
-    
+
     # Restore the best found model during validation
     #model.load_weights(fname + ".h5")
 
@@ -243,14 +244,14 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
     print("saving model " + fname)
     model.save_weights(fname + ".h5", overwrite=True)
     model.save_weights(fname + "_partial_" + str(int(np.sum(donechunks))) + "_" + str(i) + ".h5", overwrite=True)
-    
+
     i = i + 1
     donechunks[thisindices] += 1.0
-    
+
     histories.append([history.history,np.sum(donechunks)])
-    
+
     if np.sum(donechunks) % len(train_files) == 0:
-  
+
         model.save_weights(fname + "_" + str(int(np.sum(donechunks))) + ".h5", overwrite=True)
         np.random.shuffle(indices)
         #print(indices)
@@ -258,7 +259,7 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
 
     with open( fname + "_" + str(int(np.sum(donechunks))) + "_hist.pkl", 'wb') as file_hist:
     	pickle.dump(histories, file_hist)
-        
+
 print("Global epochs    : " + str(np.sum(donechunks)/len(train_files)))
 #print(endindices)
 print("saving final model " + fname)
