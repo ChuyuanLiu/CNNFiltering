b7661932ae23fbf0e1afe0cbfe796f25daf0ffc9
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-31 18:39:35 +0200

    probs

--- CNNFiltering/CNNAnalyze/python/layer_maps_model.py
+++ CNNFiltering/CNNAnalyze/python/layer_maps_model.py
@@ -263,12 +263,12 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
 
     train_pred = model.predict(train_input_list)
     train_acc,t_train = max_binary_accuracy(y,train_pred,n=1000)
-	train_y = (train_pred > t_train).astype(float)
-	print(train_y)
-	prob_indeces = np.where(train_y==y)
-	print(prob_indeces)
+    train_y = (train_pred > t_train).astype(float)
+    print(train_y)
+    prob_indeces = np.where(train_y==y)
+    print(prob_indeces)
     problematics = train_input_list[prob_indeces]
-	print(problematics)
+    print(problematics)
     print(len(problematics))
     print(len(problematics)/len(train_input_list))
 
