13f097f5533b37a351e79c851d8fa92c9ff02aae
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-31 18:55:54 +0200

    list(prob_indeces[0])

--- CNNFiltering/CNNAnalyze/python/layer_maps_model.py
+++ CNNFiltering/CNNAnalyze/python/layer_maps_model.py
@@ -276,8 +276,8 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
     train_y = (train_pred > t_train).astype(float)
     print(train_y)
     prob_indeces = np.where(train_y!=y)
-    print(prob_indeces[0])
-    problematics = train_input_list[[0,3]]
+    print(list(prob_indeces[0]))
+    problematics = train_input_list[list(prob_indeces[0])]
     print(problematics)
     print(len(problematics))
     print(len(problematics)/len(train_input_list))
