6403d97114a21dd2ae8d19731071fbe74686f88d
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-31 18:48:22 +0200

    No len

--- CNNFiltering/CNNAnalyze/python/layer_maps_model.py
+++ CNNFiltering/CNNAnalyze/python/layer_maps_model.py
@@ -218,7 +218,7 @@ while np.sum(donechunks) < len(train_files) * args.gepochs and (donechunks < arg
     else:
 		train_input_list = t_list
 
-    train_input_list = train_input_list[:len(train_input_list)-len(numprobs)]
+    train_input_list = train_input_list[:len(train_input_list)-numprobs]
     train_input_list = train_input_list + problematics
 
     # [X_val_hit[:,:,:,:4], X_val_hit[:,:,:,4:], X_val_info]
