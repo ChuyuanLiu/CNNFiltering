8bdb2178adc09679236600680248b65ea8cbe2ce
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-04-05 19:03:04 +0200

    Retesting debugging

--- CNNFiltering/CNNAnalyze/python/nn_models.py
+++ CNNFiltering/CNNAnalyze/python/nn_models.py
@@ -74,7 +74,7 @@ debug_files = [ debug_data + el for el in os.listdir(debug_data)]
 
 print("Loading data...")
 train_files = [remote_data + '/train/' +
-               el for el in os.listdir(remote_data + 'train/')] if not args.debug else[remote_data + '/train/' +
+               el for el in os.listdir(remote_data + 'train/')] if not args.debug else [remote_data + '/train/' +
                               el for el in os.listdir(remote_data + 'train/')][:3]
 
 indices = np.arange(len(train_files))
@@ -82,9 +82,9 @@ np.random.shuffle(indices)
 
 sizesamp = args.k if not args.debug else 1
 
-kfoldindices_val   = indices[:sizesamp]
-kfoldindices_test  = [el for el in indices if el not in kfoldindices_val]
-kfoldindices_train = [el for el in indices if el not in kfoldindices_val and el not in kfoldindices_test]
+kfoldindices_val   = indices[:sizesamp] if not args.debug else  indices[0]
+kfoldindices_test  = [el for el in indices if el not in kfoldindices_val] if not args.debug else  indices[1]
+kfoldindices_train = [el for el in indices if el not in kfoldindices_val and el not in kfoldindices_test] if not args.debug else  indices[2]
 
 train_files = np.take(train_files,kfoldindices_train)
 test_files = np.take(train_files,kfoldindices_test)
