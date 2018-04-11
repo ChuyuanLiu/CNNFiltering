38870caadc30dd62b2a1f6bacb35ebec7a9bec42
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-04-12 00:54:21 +0200

    ()Recchia

--- Validation/RecoTrack/plugins/MultiTrackValidatorCNNTracks.cc
+++ Validation/RecoTrack/plugins/MultiTrackValidatorCNNTracks.cc
@@ -1137,7 +1137,7 @@ void MultiTrackValidatorCNNTracks::analyze(const edm::Event& event, const edm::E
             goodHits = false;
           }
 
-          BaseTrackerRecHit const * bhit = dynamic_cast<BaseTrackerRecHit const *>(recHit);
+          BaseTrackerRecHit const * bhit = dynamic_cast<BaseTrackerRecHit const *>((*recHit));
 
           if (!bhit)
             continue;
