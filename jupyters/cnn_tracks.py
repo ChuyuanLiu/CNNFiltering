8751773440d6f851cac5afab7b7faede62801a99
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-05-09 15:58:04 +0200

    ciao

--- jupyters/cnn_tracks.py
+++ jupyters/cnn_tracks.py
@@ -154,9 +154,10 @@ for step in range(args.k_steps):
     train_input_list = [X_track, X_info]
     val_input_list = [X_val_track, X_val_info]
 
-    print("Model loaded")
-    model = adam_small_doublet_model(train_input_list[0].shape[-1],n_labels=2)
 
+    model = adam_small_doublet_model(train_input_list[0].shape[-1],n_labels=2)
+    print("Model loaded")
+    
     callbacks = [
             EarlyStopping(monitor='val_acc', patience=args.patience),
             ModelCheckpoint(args.log_dir + str(t_now) + "_" + str(step) + "_" + args.name + "_test_last.h5", save_best_only=True,
