05392c043be4083749f656440324374f5807fa98
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-06-16 19:59:21 +0200

    New Branch

--- Validation/RecoTrack/plugins/SealModule.cc
+++ Validation/RecoTrack/plugins/SealModule.cc
@@ -2,18 +2,12 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "Validation/RecoTrack/interface/MultiTrackValidator.h"
-#include "Validation/RecoTrack/interface/MultiTrackValidatorCNNTp.h"
-#include "Validation/RecoTrack/interface/MultiTrackValidatorCNN.h"
 #include "Validation/RecoTrack/interface/MultiTrackValidatorGenPs.h"
-#include "Validation/RecoTrack/interface/MultiTrackValidatorCNNTracks.h"
 
 #include "Validation/RecoTrack/interface/SiStripTrackingRecHitsValid.h"
 #include "Validation/RecoTrack/interface/SiPixelTrackingRecHitsValid.h"
 
 DEFINE_FWK_MODULE(MultiTrackValidator);
-DEFINE_FWK_MODULE(MultiTrackValidatorCNN);
-DEFINE_FWK_MODULE(MultiTrackValidatorCNNTp);
-DEFINE_FWK_MODULE(MultiTrackValidatorCNNTracks);
 DEFINE_FWK_MODULE(MultiTrackValidatorGenPs);
 DEFINE_FWK_MODULE(SiStripTrackingRecHitsValid);
 DEFINE_FWK_MODULE(SiPixelTrackingRecHitsValid);
