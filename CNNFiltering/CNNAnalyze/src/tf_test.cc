4e7fb7ed51fcdf91f67ea56356dd2daa3a4ea31e
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-09-12 11:34:30 +0200

    Minor fixes

--- CNNFiltering/CNNAnalyze/src/tf_test.cc
+++ CNNFiltering/CNNAnalyze/src/tf_test.cc
@@ -229,9 +229,9 @@ CNN_TF_Test::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       hits.push_back(lIt->doublets().hit(i, HitDoublets::inner)); //TODO CHECK EMPLACEBACK
       hits.push_back(lIt->doublets().hit(i, HitDoublets::outer));
 
-      float x = (hits[j]->hit()->globalState()).position.x();
-      float y = (hits[j]->hit()->globalState()).position.y();
-      float z = (hits[j]->hit()->globalState()).position.z();
+      float x = (hits[0]->hit()->globalState()).position.x();
+      float y = (hits[0]->hit()->globalState()).position.y();
+      float z = (hits[0]->hit()->globalState()).position.z();
 
       // Load graph
       tensorflow::setLogging("3");
@@ -243,15 +243,15 @@ CNN_TF_Test::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       tensorflow::Tensor inputY(tensorflow::DT_FLOAT, {});
 
 
-      inputX.scalar<float>()() = hitPars[j][0];
-      inputY.scalar<float>()() = hitPars[j][1];
+      inputX.scalar<float>()() = x;
+      inputY.scalar<float>()() = y;
 
       std::vector<tensorflow::Tensor> outputs;
       tensorflow::run(session, { { "x_const", inputX }, { "y_const", inputY } },
                     { "x_y_sum" }, &outputs);
 
       std::cout << outputs[0].DebugString() << std::endl;
-      std::cout << hitPars[j][2] << std::endl;
+      std::cout << z << std::endl;
 
 
       }
