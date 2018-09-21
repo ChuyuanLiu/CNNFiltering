be86102bc0c68b23f4c98e6b973919e6bfbb2987
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-09-21 15:46:28 +0200

    17220

--- RecoTracker/TkHitPairs/plugins/HitPairEDProducer.cc
+++ RecoTracker/TkHitPairs/plugins/HitPairEDProducer.cc
@@ -188,7 +188,7 @@ namespace {
         for(int j = 0; j < 2; ++j)
         {
 
-          int padOffset = layerIds[j] * padSize * padSize + j * padSize * padSize *cnnLayers;
+          int padOffset = layerIds[j] * padSize * padSize + j * padSize * padSize * cnnLayers;
 
           vLab[iLab + infoOffset] = (float)(siHits[j]->globalState()).position.x(); iLab++;
           vLab[iLab + infoOffset] = (float)(siHits[j]->globalState()).position.y(); iLab++;
@@ -295,7 +295,7 @@ namespace {
           for (int k = 0; k < thisCluster->size(); ++k)
           {
             int thisX = int((float)thisCluster->pixel(k).x - xC + padHalfSize);
-            int thisY = int(-(float)thisCluster->pixel(k).y + yC - padHalfSize);
+            int thisY = int(-(float)thisCluster->pixel(k).y + yC + padHalfSize);
             std::cout << "thisX = " << thisX << std::endl;
             std::cout << "thisY = " << thisY << std::endl;
             vPad[padOffset + thisX + thisY * padSize + doubOffset] = (float)thisCluster->pixel(k).adc;
